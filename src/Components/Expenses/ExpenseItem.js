f342fd4ae91ceb87a83688e0709690dbf62b290a
umerakr <ukhaliq.bese17seecs@seecs.edu.pk>
2023-02-20 18:56:59 +0300

    sample commit

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -5,9 +5,6 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const clickHandler = () => {
-    console.log('Clicked!!!!')
-  };
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
@@ -15,7 +12,6 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
